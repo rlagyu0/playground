170035e0bd30adea3f26355b8f4ec5461f19e162
DESKTOP-36NSF72\KGY <01086055223@hanmail.net>
2025-09-11 23:21:35 +0900

    add: create reactor playground

---
+++ reactor1/src/test/java/com/study/reactor/Reactor1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reactor1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
