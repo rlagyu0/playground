fcfae45d89d7171f235f43db1c2180ddc62af040
rlagyu0 <01086055223@hanmail.net>
2025-10-13 23:40:55 +0900

    create: spring security 6.x study

--- reactor2/src/main/java/com/study/reactor2/operator/createsequence/CreateEx.java
+++ reactor2/src/main/java/com/study/reactor2/operator/createsequence/CreateEx.java
@@ -3,6 +3,9 @@ package com.study.reactor2.operator.createsequence;
 /**
  * 여러건의 데이터를 비동기적으로 emit 이 가능하다.
  * generate 같이 프로그래밍 방식으로 시그널을 발생시키고 싶을 때 사용 (GenerateEx.class 참조)
+ * 
+ * https://github.com/ITVillage-Kevin/spring-reactive-02/blob/main/src/main/java/com/itvillage/section01/class04/CreateExample01.java
+ * 예제 1,2,3 참조
  */
 public class CreateEx {
 }
---
+++ springsecuritystudy/src/main/java/io/security/springsecuritystudy/SpringsecuritystudyApplication.java
@@ -0,0 +1,13 @@
+package io.security.springsecuritystudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritystudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritystudyApplication.class, args);
+	}
+
+}
